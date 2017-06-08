81fc97fe15c4141ac77117b0a3a5ae4c2cd24985
Marcin Owerczuk <muzieh@MacBook.local>
2017-06-08 19:45:26 +0100

    new component and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }		from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail.component'
 import { AppComponent }  from './app.component';
+import { HeroesComponent } from './heroes.component';
+import { RouterModule } from '@angular/router';
+import { HeroService } from './hero.service';
+
 
 @NgModule({
   imports:      [
   		BrowserModule,
-  		FormsModule
+  		FormsModule,
+      RouterModule.forRoot([
+        {
+          path: 'heroes' ,
+          component: HeroesComponent
+        }
+      ])
   ],
   declarations: [
   	AppComponent,
-  	HeroDetailComponent
+  	HeroDetailComponent,
+  	HeroesComponent,
 	],
+  providers: [
+    HeroService
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
